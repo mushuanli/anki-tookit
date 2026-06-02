a8f05b0e56b7e08100cc627cab12084cc6753aab
Rain Li <rain_li@trendmicro.com>
2026-06-02 20:38:56 +0800

    remove unused module

--- aicli/src/client/mod.rs
+++
@@ -1,7 +0,0 @@
-// src/client/mod.rs
-
-pub mod network;
-pub mod cli;
-pub mod tui;
-pub mod local_store;
-pub mod actions;
--- aicli/src/common/mod.rs
+++
@@ -1,7 +0,0 @@
-// src/common/mod.rs
-
-pub mod types;
-pub mod protocol;
-pub mod config;
-pub mod frame;
-pub mod crypto;
